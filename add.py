1fba813b05ba8c53cb8e122878be783b2cfa13f8
linis <linishaji0412@gmail.com>
2024-12-19 23:38:55 +0530

    second commit

---
+++ add.py
@@ -0,0 +1,13 @@
+# Simple program to add two numbers
+# Input: Two numbers
+# Output: Their sum
+
+# Input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Adding the numbers
+sum = num1 + num2
+
+# Displaying the result
+print(f"The sum of {num1} and {num2} is: {sum}")
